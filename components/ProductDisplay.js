11c9ba7ce89e5a508ab1aa9ee2f0879b660ee5cc
Eiyuulia <okkyprasetian@gmail.com>
2021-09-10 08:08:14 +0700

    finish

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -3,6 +3,10 @@ app.component('product-display', {
     premium: {
       type: Boolean,
       required: true
+    },
+    cart: {
+      type: Number,
+      required: true
     }
   },
   template: 
@@ -39,6 +43,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button
+        class="button"
+        @click="removeFromCart"
+        :class="{disabledButton: !inCart}"
+        :disabled="!inCart">
+        Remove from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,7 +67,10 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      removeFromCart(){
+        this.$emit('remove-from-cart')
       },
       updateVariant(index) {
           this.selectedVariant = index
@@ -77,6 +91,9 @@ app.component('product-display', {
           return 'Free'
         }
         return 2.99
+      },
+      inCart(){
+        return this.cart > 0
       }
   }
 })
